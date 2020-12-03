335871a5ef9e809ac29a9db021da897108908e57
bhagathchandran <75068600+bhagathchandran@users.noreply.github.com>
2020-12-03 02:19:58 -0700

    created generic collide function

--- sketch.js
+++ sketch.js
@@ -1,28 +1,59 @@
 var movingRect,fixedRect;
+var gameObject1
+
 function setup() {
-  createCanvas(800,400);
+  createCanvas(windowWidth,windowHeight);
   fixedRect = createSprite(200,200,50,50);
   fixedRect.shapeColor = "green";
+  fixedRect.velocityY = +5;
   movingRect = createSprite(400,200,80,30);
   movingRect.shapeColor = "green";
+  movingRect.velocityY = -5;
+  gameObject1 = createSprite(100,250,50,50);
+  gameObject1.shapeColor = "green";
+  gameObject1.velocityY = +5;
+
   }
 
 function draw() {
   background(255,255,255);  
   movingRect.x = World.mouseX;
   movingRect.y = World.mouseY;
-
-  if(movingRect.x-fixedRect.x < movingRect.width/2 + fixedRect.width/2 &&
-    fixedRect.x-movingRect.x < movingRect.width/2 + fixedRect.width/2 &&
-    movingRect.y-fixedRect.y < movingRect.height/2 + fixedRect.height/2 &&
-    fixedRect.y-movingRect.y < movingRect.height/2 + fixedRect.height/2){
-    fixedRect.shapeColor = "red";
-    movingRect.shapeColor = "red";
+  if(isTouching(gameObject1,movingRect)){
+    gameObject1.shapeColor = "blue";
+  movingRect.shapeColor = "blue";
   }
 
   else{
     movingRect.shapeColor = "green";
-    fixedRect.shapeColor = "green";
+    gameObject1.shapeColor = "green";
   }
+  bounceOff(gameObject1,movingRect);
   drawSprites();
+}
+
+function isTouching(object1,object2){
+  if(object1.x-object2.x < object1.width/2 + object2.width/2 &&
+    object2.x-object1.x < object1.width/2 + object2.width/2 &&
+    object1.y-object2.y < object1.height/2 + object2.height/2 &&
+    object2.y-object1.y < object1.height/2 + object2.height/2){
+   return true;
+  }
+
+  else{
+    return false;
+  }
+}
+
+function bounceOff(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2) {
+  object1.velocityX = object1.velocityX * (-1);
+  object2.velocityX = object2.velocityX * (-1);
+}
+if (object1.y - object2.y < object2.height/2 + object1.height/2
+  && object2.y - object1.y < object2.height/2 + object1.height/2){
+  object1.velocityY = object1.velocityY * (-1);
+  object2.velocityY = object2.velocityY * (-1);
+}
 }
